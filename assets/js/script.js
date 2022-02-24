980f6040c9a032f24e3b4df53806a193b472e820
alu1868 <alu1868@gmail.com>
2022-02-24 14:59:13 -0600

    prevent page from loading, changed varibales to be form instead of button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
-var buttonEl = document.querySelector("#save-task");
+//GOES TO DOM FILE, LOCATES ELEMENT BY ID, GIVES THEM A VARIABLE TO BE MANIPULATED LATER
+var formEl = document.querySelector("#task-form")
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
+
+//FUNCTION TO ADD ITEM TO LIST
 var createTaskHandler = function() {
+
+  //PREVENTS THE PAGE FROM RELOADING AFTER EVERY SUBMIT
+  event.preventDefault();
+
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
